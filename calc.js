6e0ff4de6c435c9eec6015385280fafddb47642a
Dinesh kumar <dkdon812002@gmail.com>
2022-06-18 09:56:54 +0530

    new repo

---
+++ calc.js
@@ -0,0 +1,20 @@
+let output = document.getElementById("display");
+function disp(num){
+	output.value += num;
+}
+function calculator()
+{
+try{
+	output.value = eval(output.value);
+}
+catch{
+	disp("invalid");
+}
+}
+function del(){
+	output.value = output.value.slice(0,-1);
+}
+function clear(){
+	output.value ="";
+}
+
